794c015cc8024e3196f8afa44c48a7ddcbcd21fe
Tristan (t4t5) <t4t5@hey.com>
2026-06-02 18:01:55 +0100

    Fix date range for discard

--- src-tauri/src/routes/caldir/discard.rs
+++ src-tauri/src/routes/caldir/discard.rs
@@ -4,7 +4,7 @@ use caldir_core::{Caldir, DateRange};
 
 pub(super) async fn handler() -> TauResult<()> {
     let caldir = Caldir::load().map_err(|e| e.to_string())?;
-    let range = DateRange::default();
+    let range = DateRange::default_sync_window();
 
     for connection in caldir.connections() {
         let connection = connection.map_err(|e| e.to_string())?;
